37557f9f5e339d8e231e86784816e4d39d1e512a
oibz <curarrehueinfo@gmail.com>
2022-05-23 22:24:26 -0400

    Commit Lunes 23

--- app/controllers/tv_shows_controller.rb
+++ app/controllers/tv_shows_controller.rb
@@ -1,18 +1,74 @@
 class TvShowsController < ApplicationController
+  before_action :set_tv_show, only: %i[ show edit update destroy ]
+
+  # GET /tv_shows or /tv_shows.json
   def index
-    @search = params[:query]
+    @tv_shows = TvShow.all
   end
 
+  # GET /tv_shows/1 or /tv_shows/1.json
+  def show
+  end
+
+  # GET /tv_shows/new
+  def new
+    @tv_show = TvShow.new
+  end
+
+  # GET /tv_shows/1/edit
+  def edit
+  end
+
+  # POST /tv_shows or /tv_shows.json
   def create
     @tv_show = TvShow.new(tv_show_params)
-    @tv_show.save
-    redirect_to tv_shows_path
+
+    respond_to do |format|
+      if @tv_show.save
+        format.html { redirect_to tv_show_url(@tv_show), notice: "La Serie se ha creado satisfactoriamente." }
+        format.json { render :show, status: :created, location: @tv_show }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @tv_show.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /tv_shows/1 or /tv_shows/1.json
+  def update
+    respond_to do |format|
+      if @tv_show.update(tv_show_params)
+        format.html { redirect_to tv_show_url(@tv_show), notice: "La Serie ha sido actualizada correctamente." }
+        format.json { render :show, status: :ok, location: @tv_show }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @tv_show.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tv_shows/1 or /tv_shows/1.json
+  def destroy
+    @tv_show.destroy
+
+    respond_to do |format|
+      format.html { redirect_to tv_shows_url, notice: "La Serie fue eliminada correctamente." }
+      format.json { head :no_content }
+    end
+  end
+
+  def search
+    @search = TvShow.where('name LIKE %?%', params[:query])
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_tv_show
+      @tv_show = TvShow.find(params[:id])
+    end
 
-  def tv_show_params
-    params.require(:tv_show).permit(:name, :summary, :release_date, :rating)
-  end
-
+    # Only allow a list of trusted parameters through.
+    def tv_show_params
+      params.require(:tv_show).permit(:name, :summary, :release_date, :rating)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+  resources :tv_shows
   get '/tv_shows', to: 'tv_shows#index'
   get '/welcome', to: 'pages#welcome'
   root 'pages#welcome'
   post '/tv_shows', to: 'tv_shows#create'
+
+  resources :tv_shows do
+    member do
+      get 'about'
+    end
+  end
+
+  resources :tv_shows do
+    collection do
+      get 'search'
+    end
+  end
 end
